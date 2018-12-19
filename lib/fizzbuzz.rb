bfcfdaa2fa9422501b0dbc6e493f274ecc5c6c3f
Tom Damant <tom@gluon.local>
2018-12-19 16:57:47 +0000

    fizzbuzz working

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz (number)
-  if number == 3
+  if number % 15 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
     'fizz'
   elsif number % 5 ==0
     'buzz'
+  else
+    number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,9 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'should return fizz when given 3' do
+  it 'should return fizz when given number divisible 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
   end
 
   it 'should return buzz when given multiple of 5' do
@@ -10,4 +11,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(20)).to eq 'buzz'
   end
 
+  it 'should return fizzbuzz when given multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+
+  it 'should return the number if not divisible by 3 or 5' do
+    expect(fizzbuzz(11)).to eq 11
+    expect(fizzbuzz(4)).to eq 4
+    expect(fizzbuzz(19)).to eq 19
+    expect(fizzbuzz(26)).to eq 26
+  end
 end
